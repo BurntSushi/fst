a23f606756e462baf9db72fe491333485f4323f3
Andrew Gallant <jamslam@gmail.com>
2015-10-14 21:28:03 -0400

    intersection

--- src/raw/merge.rs
+++ src/raw/merge.rs
@@ -6,58 +6,109 @@ use raw::build::Builder;
 use raw::{Fst, FstStream, Output};
 use Result;
 
-pub fn union_ignore_outputs<'f, W, I>(
+#[derive(Copy, Clone, Debug)]
+pub struct FstOutput {
+    pub index: usize,
+    pub output: u64,
+}
+
+pub fn union<'f, W, I>(
     bfst: &mut Builder<W>,
     fsts: I,
 ) -> Result<()>
 where W: io::Write,
       I: IntoIterator<Item=&'f Fst> {
-    let mut union = Union::new(fsts);
-    while let Some(slot) = union.pop() {
+    let mut heap = SlotHeap::new(fsts);
+    while let Some(slot) = heap.pop() {
         try!(bfst.add(slot.input()));
-        union.refill(slot);
+        heap.refill(slot);
     }
     Ok(())
 }
 
-#[derive(Copy, Clone, Debug)]
-pub struct UnionOutput {
-    pub index: usize,
-    pub output: u64,
-}
-
-pub fn union_with_outputs<'f, W, I, F>(
+pub fn union_with<'f, W, I, F>(
     bfst: &mut Builder<W>,
     fsts: I,
     mut merge: F,
 ) -> Result<()>
 where W: io::Write,
       I: IntoIterator<Item=&'f Fst>,
-      F: FnMut(&[u8], &[UnionOutput]) -> u64 {
-    let mut union = Union::new(fsts);
+      F: FnMut(&[u8], &[FstOutput]) -> u64 {
+    let mut heap = SlotHeap::new(fsts);
     let mut outs = vec![];
-    while let Some(slot) = union.pop() {
-        unsafe { outs.set_len(0); }
+    while let Some(slot) = heap.pop() {
+        outs.clear();
         outs.push(slot.fst_output());
-        while union.peek_is_duplicate(slot.input()) {
-            let slot2 = union.pop().unwrap();
+        while let Some(slot2) = heap.pop_if_equal(slot.input()) {
             outs.push(slot2.fst_output());
-            union.refill(slot2);
+            heap.refill(slot2);
         }
         try!(bfst.insert(slot.input(), merge(slot.input(), &outs)));
-        union.refill(slot);
+        heap.refill(slot);
     }
     Ok(())
 }
 
-struct Union<'f> {
+pub fn intersect<'f, W, I>(
+    bfst: &mut Builder<W>,
+    fsts: I,
+) -> Result<()>
+where W: io::Write,
+      I: IntoIterator<Item=&'f Fst> {
+    let mut heap = SlotHeap::new(fsts);
+    while let Some(slot) = heap.pop() {
+        let mut popped: usize = 1;
+        while let Some(slot2) = heap.pop_if_equal(slot.input()) {
+            heap.refill(slot2);
+            popped += 1;
+        }
+        // This term is only in the intersection if we found it in
+        // every FST given.
+        if popped == heap.num_slots() {
+            try!(bfst.add(slot.input()));
+        }
+        heap.refill(slot);
+    }
+    Ok(())
+}
+
+pub fn intersect_with<'f, W, I, F>(
+    bfst: &mut Builder<W>,
+    fsts: I,
+    mut merge: F,
+) -> Result<()>
+where W: io::Write,
+      I: IntoIterator<Item=&'f Fst>,
+      F: FnMut(&[u8], &[FstOutput]) -> u64 {
+    let mut heap = SlotHeap::new(fsts);
+    let mut outs = vec![];
+    while let Some(slot) = heap.pop() {
+        outs.clear();
+        outs.push(slot.fst_output());
+        let mut popped: usize = 1;
+        while let Some(slot2) = heap.pop_if_equal(slot.input()) {
+            outs.push(slot2.fst_output());
+            heap.refill(slot2);
+            popped += 1;
+        }
+        // This term is only in the intersection if we found it in
+        // every FST given.
+        if popped == heap.num_slots() {
+            try!(bfst.insert(slot.input(), merge(slot.input(), &outs)));
+        }
+        heap.refill(slot);
+    }
+    Ok(())
+}
+
+struct SlotHeap<'f> {
     rdrs: Vec<FstStream<'f>>,
     heap: BinaryHeap<Slot>,
 }
 
-impl<'f> Union<'f> {
-    fn new<I: IntoIterator<Item=&'f Fst>>(fsts: I) -> Union<'f> {
-        let mut u = Union {
+impl<'f> SlotHeap<'f> {
+    fn new<I: IntoIterator<Item=&'f Fst>>(fsts: I) -> SlotHeap<'f> {
+        let mut u = SlotHeap {
             rdrs: fsts.into_iter().map(Fst::stream).collect(),
             heap: BinaryHeap::new(),
         };
@@ -73,6 +124,18 @@ impl<'f> Union<'f> {
 
     fn peek_is_duplicate(&self, key: &[u8]) -> bool {
         self.heap.peek().map(|s| s.input() == key).unwrap_or(false)
+    }
+
+    fn pop_if_equal(&mut self, key: &[u8]) -> Option<Slot> {
+        if self.peek_is_duplicate(key) {
+            self.pop()
+        } else {
+            None
+        }
+    }
+
+    fn num_slots(&self) -> usize {
+        self.rdrs.len()
     }
 
     fn refill(&mut self, mut slot: Slot) {
@@ -100,8 +163,8 @@ impl Slot {
         }
     }
 
-    fn fst_output(&self) -> UnionOutput {
-        UnionOutput { index: self.idx, output: self.output.value() }
+    fn fst_output(&self) -> FstOutput {
+        FstOutput { index: self.idx, output: self.output.value() }
     }
 
     fn input(&self) -> &[u8] {
@@ -114,11 +177,8 @@ impl Slot {
 
     fn set_input(&mut self, input: &[u8]) {
         let addcap = input.len().checked_sub(self.input.len()).unwrap_or(0);
-        self.input.reserve(addcap);
-        unsafe { self.input.set_len(input.len()); }
-        for (dst, &byte) in self.input.iter_mut().zip(input.iter()) {
-            *dst = byte;
-        }
+        self.input.clear();
+        self.input.extend(input);
     }
 
     fn set_output(&mut self, output: Output) {
@@ -145,39 +205,39 @@ mod tests {
     use raw::build::Builder;
     use raw::tests::{fst_map, fst_set, fst_inputstrs_outputs, fst_input_strs};
     use raw::Fst;
-    use super::{union_ignore_outputs, union_with_outputs};
+    use super::{intersect, intersect_with, union, union_with};
 
     fn s(string: &str) -> String { string.to_owned() }
 
     #[test]
-    fn basic_set() {
+    fn union_set() {
         let set1 = fst_set(&["a", "b", "c"]);
         let set2 = fst_set(&["x", "y", "z"]);
 
         let mut bfst = Builder::memory();
-        union_ignore_outputs(&mut bfst, &[set1, set2]).unwrap();
+        union(&mut bfst, &[set1, set2]).unwrap();
         let fst = Fst::from_bytes(bfst.into_inner().unwrap()).unwrap();
         assert_eq!(fst_input_strs(&fst), vec!["a", "b", "c", "x", "y", "z"]);
     }
 
     #[test]
-    fn basic_set_dupes() {
+    fn union_set_dupes() {
         let set1 = fst_set(&["aa", "b", "cc"]);
         let set2 = fst_set(&["b", "cc", "z"]);
 
         let mut bfst = Builder::memory();
-        union_ignore_outputs(&mut bfst, &[set1, set2]).unwrap();
+        union(&mut bfst, &[set1, set2]).unwrap();
         let fst = Fst::from_bytes(bfst.into_inner().unwrap()).unwrap();
         assert_eq!(fst_input_strs(&fst), vec!["aa", "b", "cc", "z"]);
     }
 
     #[test]
-    fn basic_map() {
+    fn union_map() {
         let map1 = fst_map(vec![("a", 1), ("b", 2), ("c", 3)]);
         let map2 = fst_map(vec![("x", 1), ("y", 2), ("z", 3)]);
 
         let mut bfst = Builder::memory();
-        union_with_outputs(
+        union_with(
             &mut bfst,
             &[map1, map2],
             |_, os| os.iter().fold(0, |a, b| a + b.output),
@@ -192,13 +252,13 @@ mod tests {
     }
 
     #[test]
-    fn basic_map_dupes() {
+    fn union_map_dupes() {
         let map1 = fst_map(vec![("aa", 1), ("b", 2), ("cc", 3)]);
         let map2 = fst_map(vec![("b", 1), ("cc", 2), ("z", 3)]);
         let map3 = fst_map(vec![("b", 1)]);
 
         let mut bfst = Builder::memory();
-        union_with_outputs(
+        union_with(
             &mut bfst,
             &[map1, map2, map3],
             |_, os| os.iter().fold(0, |a, b| a + b.output),
@@ -209,5 +269,58 @@ mod tests {
             vec![
                 (s("aa"), 1), (s("b"), 4), (s("cc"), 5), (s("z"), 3),
             ]);
+    }
+
+    #[test]
+    fn intersect_set() {
+        let set1 = fst_set(&["a", "b", "c"]);
+        let set2 = fst_set(&["x", "y", "z"]);
+
+        let mut bfst = Builder::memory();
+        intersect(&mut bfst, &[set1, set2]).unwrap();
+        let fst = Fst::from_bytes(bfst.into_inner().unwrap()).unwrap();
+        assert_eq!(fst_input_strs(&fst), Vec::<&str>::new());
+    }
+
+    #[test]
+    fn intersect_set_dupes() {
+        let set1 = fst_set(&["aa", "b", "cc"]);
+        let set2 = fst_set(&["b", "cc", "z"]);
+
+        let mut bfst = Builder::memory();
+        intersect(&mut bfst, &[set1, set2]).unwrap();
+        let fst = Fst::from_bytes(bfst.into_inner().unwrap()).unwrap();
+        assert_eq!(fst_input_strs(&fst), vec!["b", "cc"]);
+    }
+
+    #[test]
+    fn intersect_map() {
+        let map1 = fst_map(vec![("a", 1), ("b", 2), ("c", 3)]);
+        let map2 = fst_map(vec![("x", 1), ("y", 2), ("z", 3)]);
+
+        let mut bfst = Builder::memory();
+        intersect_with(
+            &mut bfst,
+            &[map1, map2],
+            |_, os| os.iter().fold(0, |a, b| a + b.output),
+        ).unwrap();
+        let fst = Fst::from_bytes(bfst.into_inner().unwrap()).unwrap();
+        assert_eq!(fst_inputstrs_outputs(&fst), Vec::<(String, u64)>::new());
+    }
+
+    #[test]
+    fn intersect_map_dupes() {
+        let map1 = fst_map(vec![("aa", 1), ("b", 2), ("cc", 3)]);
+        let map2 = fst_map(vec![("b", 1), ("cc", 2), ("z", 3)]);
+        let map3 = fst_map(vec![("b", 1)]);
+
+        let mut bfst = Builder::memory();
+        intersect_with(
+            &mut bfst,
+            &[map1, map2, map3],
+            |_, os| os.iter().fold(0, |a, b| a + b.output),
+        ).unwrap();
+        let fst = Fst::from_bytes(bfst.into_inner().unwrap()).unwrap();
+        assert_eq!(fst_inputstrs_outputs(&fst), vec![(s("b"), 4)]);
     }
 }
